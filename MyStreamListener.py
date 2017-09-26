9d56353efb211e4379edfc7240e5d0e882986c7f
Foster Song <fsong95@gmail.com>
2017-09-26 16:05:35 -0400

    more matching phrases

--- MyStreamListener.py
+++ MyStreamListener.py
@@ -11,29 +11,30 @@ api = tweepy.API(auth)  # create an API object
 class MyStreamListener(tweepy.StreamListener):
 
     def on_data(self, data):
-        #print(status.text)
-        print(data)
 
         twt = json.loads(data)
-
         filename = "/Users/foster/Documents/Dartmouth/17F/CS98/hack-a-thing-2-caremorebot/carebar.gif"
-        #print(status.id)
 
-        # old_id = status.id
-
-        print(twt['id'])
-
+        # create message, concat with user name
         user = "@" + str(twt['user']['screen_name'])
-        msg = 'Hey ' + user + ', did you mean "I couldn\'t care less"?'
+        msg = 'Hey ' + user + ', did you mean "couldn\'t care less"?'
 
-        # matches = ['I could care less', 'could care less', 'I really could care less']
+        # check for matching phrases
+        matches = ['I could care less', 'could care less', 'I really could care less', 'I could really care less']
+        matched = False
+
+        for match in matches:
+            if twt['text'].find(match) >= 0:
+                matched = True
+                continue
 
         txt = 'I could care less'
         print(twt['text'])
 
         print(str(twt['retweeted']) + " \n " + str(type(twt['retweeted'])))
 
-        if twt['text'].find(txt) >= 0 and twt['retweeted'] is False:
+        # if twt['text'].find(txt) >= 0 and twt['retweeted'] is False:
+        if matched and twt['retweeted'] is False:
             if twt['text'].find('RT @') < 0:
                 print("tweeting now")
                 api.update_with_media(filename, status=msg, in_reply_to_status_id=int(twt['id']))
